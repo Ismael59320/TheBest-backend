49589df4430a5735e6cce55ca02ea491dc1c7c2b
Fethi <khalifethi@live.fr>
2024-12-10 11:38:49 +0100

    CALL_API

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 var express = require('express');
+var app = express(); 
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -7,20 +8,20 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const placieRouter = require('./routes/placie');
 require('./models/connection');
-
 const mongoose = require('mongoose');
+const googleAPIRouter = require('./routes/API_GOOGLE');
 
-var app = express();
 const cors = require('cors');
 app.use(cors());
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', googleAPIRouter); 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/placies', placieRouter);
+
 module.exports = app;
