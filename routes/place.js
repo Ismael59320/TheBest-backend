5f6af125b0fc90ac6e2a95530f6e2faf814eadc1
Ismael59320 <slr720@outlook.fr>
2024-12-12 10:23:07 +0100

    ok

--- routes/place.js
+++ routes/place.js
@@ -1,10 +1,6 @@
 var express = require("express");
 var router = express.Router();
 const Place = require("../models/place");
-require("dotenv").config();
-
-const MongoURL = process.env.MongoURL;
-const KEY_API_APIFY = process.env.KEY_API_APIFY;
 
 // const nearbyPlaces = await Place.find({
 //   location: {
