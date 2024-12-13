67f610d8eef63bef2d2ef2cf87d8039c9a148f88
Marine <marine.schuermans@hotmail.fr>
2024-12-13 13:56:18 +0100

    route modifiée

--- routes/users.js
+++ routes/users.js
@@ -42,7 +42,7 @@ router.post("/signin", (req, res) => {
   if (!checkBody(req.body, ["username", "password"])) {
     res.json({ result: false, error: "Missing or empty fields" });
     return;
-  }
+  } 
 
   User.findOne({ username: req.body.username }).then((data) => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
