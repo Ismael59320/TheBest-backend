aa25bac1e3b07ea8cd1f6b885a223ace58da3b66
Marine <marine.schuermans@hotmail.fr>
2024-12-18 18:07:19 +0100

    route get reviews by place id okkk

--- routes/reviews.js
+++ routes/reviews.js
@@ -25,4 +25,48 @@ router.post('/', (req, res) => {
     });
 });
 
+// router.get("/:place_id", (req,res) => {
+//     const place_id = req.params.place_id
+//     const review = Review.find(req.params.place_id)
+
+//     if (review) {
+//         res.json({result : true, review})
+//     } else {
+//         res.json({result: false})
+//     }
+//     // Review.find({place_id: req.body.place_id}).then((data) => {
+
+        
+
+//     //     const formattedReviews = data.map(review => ({
+//     //         user: review.user,
+//     //         place_id: review.place_id,
+//     //         rating: review.rating,
+//     //         username: review.username,
+//     //         text: review.text,
+//     //         photo: review.photo,
+//     //         created: review.created
+//     //     }))
+
+//     //     res.json({result: true, formattedReviews})
+//     // })
+// })
+
+router.get("/:place_id", async (req, res) => {
+    try {
+        const place_id = req.params.place_id;
+
+        // Recherche des reviews correspondant au place_id
+        const reviews = await Review.find({ place_id: place_id });
+
+        if (reviews && reviews.length > 0) {
+            res.json({ result: true, reviews });
+        } else {
+            res.json({ result: false, message: "Aucun avis Bester pour le moment, soyez le premier" });
+        }
+    } catch (error) {
+        res.status(500).json({ result: false, error: "Erreur de recupération des avis" });
+    }
+});
+
 module.exports = router;
